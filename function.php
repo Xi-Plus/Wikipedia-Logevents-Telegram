11b9d2d7affe35dc53a2aa509131b7f3d0710fff
xiplus <huangxuanyuxiplus@gmail.com>
2018-01-20 14:09:09 +0800

    add rightname bureaucrat, oversight

--- function.php
+++ function.php
@@ -30,10 +30,12 @@ function protectparams($text) {
 function rightparams(&$item, $key) {
 	$name = [
 		'autoreviewer' => '巡查豁免者',
+		'bureaucrat' => '行政員',
 		'confirmed' => '已確認的使用者',
 		'flood' => '機器使用者',
 		'ipblock-exempt' => 'IP 封鎖例外',
 		'massmessage' => '大量訊息傳送者',
+		'oversight' => '監督員',
 		'patroller' => '巡查員',
 		'rollbacker' => '回退員',
 		'sysop' => '管理員'
